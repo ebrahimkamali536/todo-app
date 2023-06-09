ae7860ebca12fce7f7008a0fe075d1cf8d7775df
ebrahim kamali <ebrahimkamali536@gmail.com>
2023-06-09 22:16:25 +0330

    create react app using vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+import "./App.css"
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default App;
